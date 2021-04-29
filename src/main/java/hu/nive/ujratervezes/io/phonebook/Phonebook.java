c6aac13f275a9edc641abb256c32f7a82f8ade58
kercse <69111507+kercse@users.noreply.github.com>
2021-04-29 12:48:25 +0200

    finally working last minute

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,30 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class Phonebook {
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+
+        File file = new File(output);
+
+        try {
+            if (contacts == null) {
+                throw new IllegalArgumentException();
+            }
+
+            BufferedWriter bufferWriter = new BufferedWriter(new FileWriter(file));
+            for (Map.Entry<String, String> entry : contacts.entrySet()) {
+                bufferWriter.write(entry.getKey() + ": " + entry.getValue());
+                bufferWriter.newLine();
+            }
+
+            bufferWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
